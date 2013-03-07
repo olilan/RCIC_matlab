a42409025124a436b53d72cde3114c5692e585a5
Oliver Langner <olilan@me.com>
2013-03-08 00:33:15 +0100

    Added new main interface file.

---
+++ rcic_main.m
@@ -0,0 +1,22 @@
+%Change and store this setting file for each experiment.
+
+%% image generation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%make config settings for image generation
+gen_cfg = struct( ...
+    'genImg', True, ...             %render images
+    'bf', 'rafd_average.jpg', ...   %base face image
+    'blur', False, ...              %don't blur base face
+    'symm', True, ...               %generate also inverse image
+    'prefix', 'teststim', ...       %name prefix for images ...
+    'nrS', 5, ...                   %number of stimuli
+    'mask', [] ...                  %no masking of stimuli
+    );
+
+%uncomment next line and place needed seed, in case you need to recreate
+%images from a known state of the random generator
+%gen_cfg.seed = 123;
+
+%generate stimuli
+rcic_generate_stimuli(gen_cfg);
+
